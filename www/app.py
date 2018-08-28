bab85853e7eba8d4e6f5b636a83f9dc4d9cec279
jonxli <49653266@qq.com>
2018-08-28 10:13:03 +0800

    add author

--- www/app.py
+++ www/app.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__='Jon Li'
+
+'''
+async web application
+'''
+
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
